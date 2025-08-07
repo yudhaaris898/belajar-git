ccf74db7a197ca29628bc3c2e331970afd7b2a06
Yudha Aris Kristanto <yudhaaris898@gmail.com>
2025-08-07 19:27:24 +0700

    merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,8 @@ console.log(add(1, 2));
 const subtract = (a, b) => a - b;
 console.log(subtract(5, 2));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(2, 2));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(2, 2));
 
 const divide = (a, b) => a / b;
 console.log(divide(4, 2));
